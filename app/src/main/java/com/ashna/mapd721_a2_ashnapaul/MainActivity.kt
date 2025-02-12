a8f01e75f30b909697fe6c27af45f681698ac8f9
ashnapaularackal <ashnapaularackal@gmail.com>
2025-02-11 22:35:20 -0500

    Request Permissions

--- app/src/main/java/com/ashna/mapd721_a2_ashnapaul/MainActivity.kt
+++ app/src/main/java/com/ashna/mapd721_a2_ashnapaul/MainActivity.kt
@@ -1,35 +1,47 @@
 package com.ashna.mapd721_a2_ashnapaul
 
 import android.os.Bundle
+import android.webkit.PermissionRequest
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.records.HeartRateRecord
 import com.ashna.mapd721_a2_ashnapaul.ui.theme.MAPD721A2AshnaPaulTheme
+import java.security.Permission
 
 class MainActivity : ComponentActivity() {
+    private lateinit var healthConnectClient: HealthConnectClient
+
+    private val healthPermissions = setOf(
+        Permission.createWritePermission(HeartRateRecord::class),
+        Permission.createReadPermission(HeartRateRecord::class)
+    )
+
+    private var heartRateHistory: List<String> = listOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MAPD721A2AshnaPaulTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+
+        healthConnectClient = HealthConnectClient.getOrCreate(this)
+        requestHealthPermissions()
+
     }
 }
 
+private fun requestHealthPermissions() {
+    val request = PermissionRequest(healthPermissions)
+    val permissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            if (permissions.containsValue(false)) {
+                Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    permissionLauncher.launch(request.permissions.toTypedArray())
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
